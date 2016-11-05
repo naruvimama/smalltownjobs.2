aa7406fcb6d1df5791687e26c3e0d6866a9dca79
Prelang Builder <builder@prelang.com>
2016-11-05 01:16:28 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Recruiter' to allow for create, update

--- app/admin/recruiter.rb
+++ app/admin/recruiter.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Recruiter do
+  permit_params :name, :picture, :location, :headline, :summary, :contact_email, :contact_phone, :specialization, :professional_profile, :stats
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
