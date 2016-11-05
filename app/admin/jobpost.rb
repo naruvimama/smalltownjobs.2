a779e98ca33c0ddf12003aa29b53f48615031fc9
Prelang Builder <builder@prelang.com>
2016-11-05 01:16:32 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Jobpost' to allow for create, update

--- app/admin/jobpost.rb
+++ app/admin/jobpost.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Jobpost do
+  permit_params :headline, :job_title, :location, :summary, :fields, :seniority, :remote, :commute, :skills, :nice_to_have, :requirements, :stats, :active, :application_deadline, :open_ended_start, :decission_deadline, :no_applicants, :average_rating, :rating_histogram
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
