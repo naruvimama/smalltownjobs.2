6f180b21c9f9146b0b37fae51a871e455c765903
Prelang Builder <builder@prelang.com>
2016-11-05 01:16:15 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Town' to allow for create, update

--- app/admin/town.rb
+++ app/admin/town.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Town do
+  permit_params :name, :state, :country, :nearest_town, :distance_to_nearest_town_miles, :distance_to_nearest_town_km, :tags, :child_friendly, :bike_friendly, :nature, :housing_price_index, :living_cost_index, :commute_reliability, :livability_index, :air_polution_index, :hazard, :recreation, :emergency_care, :facilities, :tags
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
