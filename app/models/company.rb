266a5f2d23f776d3a4881c59b4732967b0ad35a7
Prelang Builder <builder@prelang.com>
2016-11-05 01:16:12 +0000

    Defined validation for attribute 'career_section' on model 'Company'

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,5 @@ class Company < ActiveRecord::Base
   has_many :users
   has_many :mailboxes
   validates_formatting_of :website, using: :url
+  validates_formatting_of :career_section, using: :url
 end
